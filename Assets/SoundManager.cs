6981906c0fcdea75816624af8cbdc2276ce7f20c
Fiona Chye <fiona_chye@hotmail.ca>
2017-06-21 14:57:53 -0400

    first commit

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource playerFxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayerPlaySingle(AudioClip clip)
+    {
+        playerFxSource.clip = clip;
+        playerFxSource.Play();
+    }
+
+    public void PlayerPlayDelayed(AudioClip clip, float delay)
+    {
+        playerFxSource.clip = clip;
+        playerFxSource.PlayDelayed(delay);
+    }
+}
